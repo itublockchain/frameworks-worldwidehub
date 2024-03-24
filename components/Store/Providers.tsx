ab1556f524a1cfb27b32f6f872acf90886b437f5
Ataberk <ataberkcekic@hotmail.com.tr>
2024-03-24 07:19:39 +0300

    fix

--- components/Store/Providers.tsx
+++ components/Store/Providers.tsx
@@ -1,26 +1,32 @@
 "use client";
 
-import { PrivyProvider, usePrivy } from '@privy-io/react-auth';
+import { PrivyProvider, usePrivy } from "@privy-io/react-auth";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const { login, ready, authenticated } = usePrivy();
   const disableLogin = !ready || (ready && authenticated);
-  
+
   return (
-    <PrivyProvider appId="clu45qg6q0k2tklm62opgvbp8" config={{
-      loginMethods : ["wallet", "farcaster", "email", "google"],
-      appearance : {
-        logo : "../public/logo.png",
-        theme : "light",
-        accentColor : "green",
-        showWalletLoginFirst : true,
-        walletList : ["metamask", "phantom", "walletconnect", "walletlink", "coinbase"]
-      },
-      embeddedWallets: {
-        createOnLogin: 'users-without-wallets',
+    <PrivyProvider
+      appId="clu45qg6q0k2tklm62opgvbp8"
+      config={{
+        loginMethods: ["wallet", "farcaster", "email", "google"],
+        appearance: {
+          logo: "../public/logo.png",
+          theme: "light",
+          showWalletLoginFirst: true,
+          walletList: ["metamask", "phantom"],
+        },
+        embeddedWallets: {
+          createOnLogin: "users-without-wallets",
+        },
+      }}
+    >
+      {
+        <button disabled={false} onClick={login}>
+          Log In
+        </button>
       }
-    }}>{
-      <button disabled={false} onClick={login}>Log In</button>
-    }</PrivyProvider>
+    </PrivyProvider>
   );
 }
